911c74fdc2135a094c74aedba999913c62985941
deepkhanpara <120081895+deep764@users.noreply.github.com>
2024-08-01 20:15:30 +0530

    Delete task directory

--- task/Triangle.kt
+++
@@ -1,8 +0,0 @@
-fun main()
-{
-    val w=6
-    val h=3
-
-    val triangleArea = (w*h)/2
-    println("Area of Triangle is: $triangleArea")
-}
--- task/simple Interest.kt
+++
@@ -1,9 +0,0 @@
-fun main()
-{
-    val p=600
-    val n=3
-    val r=4
-
-    val si = p*n*r/100
-    println("Area of Simple Interest is: $si")
-}
