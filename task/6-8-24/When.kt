932e51145f48231191a3b39e2f8555b8e0c095b9
deepkhanpara <120081895+deep764@users.noreply.github.com>
2024-08-06 22:04:21 +0530

    Add files via upload

---
+++ task/6-8-24/When.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    println("Enter Any Num")
+    var num = readLine()!!.toInt()
+
+    when (num) {
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+
+    }
+}
+
