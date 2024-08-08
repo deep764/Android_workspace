7461856288fc543650681b834ad82143d5d2d3a7
deepkhanpara <120081895+deep764@users.noreply.github.com>
2024-08-08 20:50:40 +0530

    Add files via upload

---
+++ task/8-8-24/while2.kt
@@ -0,0 +1,23 @@
+package test
+
+fun main()
+{
+    var i=1
+    while(i<=5)
+    {
+        i++
+        var j=5
+            while(j>=1)
+            {
+                if(j>i)
+                {
+                    print("")
+                }
+                else
+                {
+                    print("*")
+                }
+                println()
+            }
+    }
+}
