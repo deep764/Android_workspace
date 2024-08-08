5cfa80efb84d3ede231a329f576055ae41373101
deepkhanpara <120081895+deep764@users.noreply.github.com>
2024-08-08 20:34:03 +0530

    Add files via upload

---
+++ task/8-8-24/whlie.1.kt
@@ -0,0 +1,19 @@
+package test
+
+fun main()
+{
+    var i=0
+    while(i<=4)
+    {
+        i++
+        var j = 1
+        while (j <= i)
+        {
+            print(j)
+            j++
+
+        }
+        println()
+    }
+
+}
